02ae4c5112b0c9dafd0442ba81c25afdedbe7237
Dylan <dcren2010@gmail.com>
2021-09-06 02:50:56 -0400

    add reload icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import SearchBook from './components/SearchBook/SearchBook';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import reload from '../src/images/reload.png'
 
 function App() {
 	return (
 		<Router>
 			<div className='container'>
-				<h1 className='title'>Online Book Search</h1>
+				<h1 onClick={() => window.location.reload()} className='title'>Online Book Search
+				<img className="reload" src={reload} alt="reload" />
+				</h1>
 				<Switch>
 					<Route exact path='/'>
 						<SearchBook />
