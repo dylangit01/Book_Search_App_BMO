be1d4adbf854fc6b5652c2968bc9461297ecda38
Dylan <dcren2010@gmail.com>
2021-09-19 13:15:25 -0400

    add App.test.js file

---
+++ client/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './redux/reducers';
+
+const renderWithRedux = (
+	component,
+	{ initialState, store = createStore(reducers, compose(applyMiddleware(thunk))) } = {}
+) => {
+	return {
+		...render(<Provider store={store}>{component}</Provider>),
+	};
+};
+
+test('renders learn react link', () => {
+	renderWithRedux(<App />);
+	const linkElement = screen.getByText(/Book Search Master/i);
+	expect(linkElement).toBeInTheDocument();
+	expect(linkElement.textContent).toBe('Book Search Master');
+});
